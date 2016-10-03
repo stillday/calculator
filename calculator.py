46d3076a05e735ad0caaa0baa77d8eefd706cd9d
Jan Drenkhahn <jan-dennisd@outlook.at>
2016-10-03 13:02:53 +0200

    with button

--- calculator.py
+++ calculator.py
@@ -12,13 +12,6 @@ xtext.pack()
 x = Tkinter.Entry(window)
 x.pack()
 
-# Operation Eingabe Text
-operationtext = Tkinter.Label(window, text="Choose math operation (+, -, *, /): ")
-operationtext.pack()
-#Operation Eingabe
-operation = Tkinter.Entry(window)
-operation.pack()
-
 # Y Eingabe Text
 ytext = Tkinter.Label(window, text="Enter the value for Y: ")
 ytext.pack()
@@ -26,22 +19,31 @@ ytext.pack()
 y = Tkinter.Entry(window)
 y.pack()
 
-def calculation():
-    if operation.get() == "I'm a hero":
-        kalk = "Yes you are a super hero"
-
-    elif operation.get() == "+":
-        kalk = int(x.get()) + int(y.get())
-    elif operation.get() == "-":
-        kalk = int(x.get()) - int(y.get())
-    elif operation.get() == "*":
-        kalk = int(x.get()) * int(y.get())
-    elif operation.get() == "/":
-        kalk = int(x.get()) / int(y.get())
-
+def plus():
+    kalk = int(x.get()) + int(y.get())
     tkMessageBox.showinfo("Result", kalk)
 
-submit = Tkinter.Button(window, text = "Berechnen", command=calculation)
-submit.pack()
+def minus():
+    kalk = int(x.get()) - int(y.get())
+    tkMessageBox.showinfo("Result", kalk)
+
+def multiplizieren():
+    kalk = int(x.get()) * int(y.get())
+    tkMessageBox.showinfo("Result", kalk)
+
+def dividieren():
+    kalk = int(x.get()) / int(y.get())
+    tkMessageBox.showinfo("Result", kalk)
+
+#Button
+plus_button = Tkinter.Button(window, text = "+", command=plus)
+plus_button.pack()
+minus_button = Tkinter.Button(window, text = "-", command=minus)
+minus_button.pack()
+multi_button = Tkinter.Button(window, text = "*", command=multiplizieren)
+multi_button.pack()
+divi_button = Tkinter.Button(window, text = "/", command=dividieren)
+divi_button.pack()
+
 
 window.mainloop()
